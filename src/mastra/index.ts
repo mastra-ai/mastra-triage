3eaacfe184a3dc68ae995192cb609284e5091f4a
wardpeet <ward@coding-tech.com>
2025-09-05 10:24:47 +0200

    add auth

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -3,8 +3,9 @@ import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 import { triageAgent } from './agents/triage';
 import { discordToGithubWorkflow } from './workflows/discordToGithub';
-// import { createGithubIssueWorkflow } from './workflows/discordToGithub/createGithubIssue';
+import { createGithubIssueWorkflow } from './workflows/discordToGithub/createGithubIssue';
 import { discordAnalysisWorkflow } from './workflows/analysis';
+import { MastraJwtAuth } from '@mastra/auth';
 
 export const mastra = new Mastra({
   agents: { triageAgent },
@@ -14,11 +15,16 @@ export const mastra = new Mastra({
   }),
   workflows: {
     discordToGithubWorkflow,
-    // createGithubIssueWorkflow,
+    createGithubIssueWorkflow,
     discordAnalysisWorkflow,
   },
   logger: new PinoLogger({
     name: 'Mastra',
     level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
   }),
+  server: {
+    experimental_auth: new MastraJwtAuth({
+      secret: process.env.MASTRA_JWT_SECRET,
+    }),
+  },
 });
